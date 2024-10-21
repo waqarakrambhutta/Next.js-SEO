b599ca1b109f8a3aacd15dbd4e40389d026f8ed2
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2024-10-22 02:21:14 +0500

    Boilerplate

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: {
+    default: "My Awesome Blog",
+    template: "%s - My Awesome Blog",
+  },
+  description: "Come and read my awesome articles!",
+  twitter: {
+    card: "summary_large_image",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Header />
+        <main className="p-5">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ src/app/posts/[postId]/page.tsx
@@ -0,0 +1,63 @@
+import ClapButton from "@/components/ClapButton";
+import { delay } from "@/lib/utils";
+import { BlogPost, BlogPostsResponse } from "@/models/BlogPost";
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+import { cache } from "react";
+
+interface BlogPostPageProps {
+  params: { postId: string };
+}
+
+export async function generateStaticParams() {
+  const response = await fetch("https://dummyjson.com/posts");
+  const { posts }: BlogPostsResponse = await response.json();
+
+  return posts.map(({ id }) => id);
+}
+
+// Manually deduplicate requests if not using fetch
+// const getPost = cache(async (postId: string) => {
+//   const post = await prisma.post.findUnique(postId);
+//   return post;
+// })
+
+export async function generateMetadata({
+  params: { postId },
+}: BlogPostPageProps): Promise<Metadata> {
+  const response = await fetch(`https://dummyjson.com/posts/${postId}`);
+  const post: BlogPost = await response.json();
+
+  return {
+    title: post.title,
+    description: post.body,
+    // openGraph: {
+    //   images: [
+    //     {
+    //       url: post.imageUrl
+    //     }
+    //   ]
+    // }
+  };
+}
+
+export default async function BlogPostPage({
+  params: { postId },
+}: BlogPostPageProps) {
+  const response = await fetch(`https://dummyjson.com/posts/${postId}`);
+  const { title, body }: BlogPost = await response.json();
+
+  if (response.status === 404) {
+    notFound();
+  }
+
+  await delay(1000);
+
+  return (
+    <article className="max-w-prose m-auto space-y-5">
+      <h1 className="text-3xl text-center font-bold">{title}</h1>
+      <p className="text-lg">{body}</p>
+      <ClapButton />
+    </article>
+  );
+}
